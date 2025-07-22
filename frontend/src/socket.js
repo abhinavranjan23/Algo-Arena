2613c2632d5c4956755b62be2cb2aa00a66c366c
abhinavranjan23 <abhinavranjan2202@gmail.com>
2025-07-22 11:43:37 +0530

    final commit

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,5 +2,5 @@ import { io } from "socket.io-client";
 
 export const socket = io("http://localhost:3000", {
   withCredentials: true,
-  //   transports: ["websocket"]
+  //   transports: ["websocket"],
 });
